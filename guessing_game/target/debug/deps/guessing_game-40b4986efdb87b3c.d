242cd5deedc22f3ac753ad8ca987655c896b90c5
Harrison <harrisonhemstreet@gmail.com>
2021-02-19 12:06:38 -0700

    added a function that does not work

---
+++ guessing_game/target/debug/deps/guessing_game-40b4986efdb87b3c.d
@@ -0,0 +1,5 @@
+/home/hhemstreet/rust-practice/guessing_game/target/debug/deps/guessing_game-40b4986efdb87b3c: src/main.rs
+
+/home/hhemstreet/rust-practice/guessing_game/target/debug/deps/guessing_game-40b4986efdb87b3c.d: src/main.rs
+
+src/main.rs:
